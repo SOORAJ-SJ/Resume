affa20a6012b91a2a8d4f38e4a388b7f3cb3f181
Sooraj M <61335345+SOORAJ-SJ@users.noreply.github.com>
2021-03-19 15:57:52 +0530

    Update sw.js

--- build/sw.js
+++ build/sw.js
@@ -11,7 +11,7 @@ self.addEventListener('install',(event)=>{
                 '/Resume/build/images/qi.jpg',
                 '/Resume/build/images/sj.png',
                 '/Resume/build/images/sjfavicon.ico',
-            ]));
+            ]);
     );
 });
 self.addEventListener('fetch',(event)=>{
